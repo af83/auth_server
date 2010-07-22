4d388abfeac5d2aee8a933c2af5d103ebdd7bd67
Virtuo <pierre@ruyssen.fr>
2010-07-22 15:28:16 +0200

    Move GET and POST to nodetk.

--- src/tests/test_auth_server.js
+++ src/tests/test_auth_server.js
@@ -12,66 +12,8 @@ var auth_server = require('auth_server')
   , http = require('http')
   , eyes = require('eyes')
   , load_data = require('../scripts/load_data').run
+  , web = require('nodetk/web')
   ;
-
-
-
-//--------------------------------------------------
-
-var REQ = function(type, url, data, callback) {
-  /* Make a request to the given URL, and call:
-   *  callback(http_code, headers, data);
-   */
-  var purl = URL.parse(url);
-  var qs = purl.query || '';
-  var client = http.createClient(purl.port || 80, purl.hostname);
-  client.addListener('error', function(err) {
-    console.log(err.message);
-    console.log(err.stack);
-  });
-  var headers = {
-    'host': purl.hostname,
-    // We want to simulate a browser:
-    'User-Agent': 'Mozilla/5.0 (X11; U; Linux i686; fr; rv:1.9.1.9) Gecko/20100401 Ubuntu/9.10 (karmic) Firefox/3.5.9',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Accept-Language': 'fr-fr,fr;q=0.8,en;q=0.5,en-us;q=0.3',
-    //'Accept-Encoding': 'gzip,deflate',
-  };
-  if(type == 'POST' || type == 'PUT') {
-    headers['content-type'] = 'application/x-www-form-urlencoded';
-  }
-  else if (data) {
-    if(qs) qs += '&';
-    qs += querystring.stringify(data);
-  }
-  var request = client.request(type, purl.pathname +'?'+ qs, headers);
-  if((type == 'POST' || type == 'PUT') && data) {
-    request.write(querystring.stringify(data), 'utf8');
-  }
-  request.end();
-  var data = '';
-  request.on('response', function(response) {
-    response.setEncoding('utf8');
-    response.on('data', function(chunk) {
-      data += chunk;
-    });
-    response.on('end', function() {
-      callback(response.statusCode, response.headers, data);
-    });
-  });
-}
-
-var GET = function(url, data, callback) {
-  if(data == {}) data = null;
-  return REQ('GET', url, data, callback);
-};
-
-var POST = function(url, data, callback) {
-  return REQ('POST', url, data, callback);
-};
-// ----------------------------------------------------------
-
-
 
 
 server.serve(9999)
@@ -103,7 +45,7 @@ exports.tests = [
 
 ['/oauth/authorize: no parameter', 2, function() {
   // no params (missing mandatory ones) should give us an error.
-  GET(authorize_url, null, get_error_checker('invalid_request'));
+  web.GET(authorize_url, null, get_error_checker('invalid_request'));
 }],
 
 
@@ -117,14 +59,14 @@ exports.tests = [
   auth_server.PARAMS.mandatory.forEach(function(param) {
     var partial_qs = extend({}, qs);
     delete partial_qs[param];
-    GET(authorize_url, partial_qs, get_error_checker('invalid_request'));
+    web.GET(authorize_url, partial_qs, get_error_checker('invalid_request'));
   });
 }],
 
 
 ['/oauth/authorize: bad client_id', 2, function() {
   // if the given client id is not in DB, error.
-  GET(authorize_url, {
+  web.GET(authorize_url, {
     client_id: "toto",
     response_type: "code",
     redirect_uri: "http://127.0.0.1:8888/login"
@@ -133,7 +75,7 @@ exports.tests = [
 
 ['/oauth/authorize: redirect_uri mismatch', 2, function() {
   // if the redirect_uri is not the same as registered: error.
-  GET(authorize_url, {
+  web.GET(authorize_url, {
     client_id: "errornot",
     response_type: "code",
     redirect_uri: "http://127.0.0.1:8888/login/wrong"
@@ -142,7 +84,7 @@ exports.tests = [
 
 ['/oauth/authorize: unsupported_response_type', 2, function() {
   // if the response_type is not an accepted value: error.
-  GET(authorize_url, {
+  web.GET(authorize_url, {
     client_id: "errornot",
     response_type: "wrong",
     redirect_uri: "http://127.0.0.1:8888/login"
@@ -152,7 +94,7 @@ exports.tests = [
 // -------------------------------------------------------------------------
 // XXX : The two following tests are NOT norm compliant, cf auth_server.js
 ['/oauth/authorize: token response_type', 1, function() {
-  GET(authorize_url, {
+  web.GET(authorize_url, {
     client_id: "errornot",
     response_type: "token",
     redirect_uri: "http://127.0.0.1:8888/login"
@@ -162,7 +104,7 @@ exports.tests = [
 }],
 
 ['/oauth/authorize: code_and_token response_type', 1, function() {
-  GET(authorize_url, {
+  web.GET(authorize_url, {
     client_id: "errornot",
     response_type: "code_and_token",
     redirect_uri: "http://127.0.0.1:8888/login"
@@ -175,7 +117,7 @@ exports.tests = [
 
 ['/oauth/authorize: ok', 1, function() {
   // if the response_type is not an accepted value: error.
-  GET(authorize_url, {
+  web.GET(authorize_url, {
     client_id: "errornot",
     response_type: "code",
     redirect_uri: "http://127.0.0.1:8888/login"
@@ -187,7 +129,7 @@ exports.tests = [
 
 
 ['authentication ok', 4, function() {
-  POST(login_url, {
+  web.POST(login_url, {
     client_id: 'errornot',
     response_type: 'code',
     redirect_uri: 'http://127.0.0.1:8888/login',
@@ -206,7 +148,7 @@ exports.tests = [
 
 
 ['authentication: wrong password', 1, function() {
-  POST(login_url, {
+  web.POST(login_url, {
     client_id: 'errornot',
     response_type: 'code',
     redirect_uri: 'http://127.0.0.1:8888/login',
@@ -220,7 +162,7 @@ exports.tests = [
 
 
 ['authentication: unknown user', 1, function() {
-  POST(login_url, {
+  web.POST(login_url, {
     client_id: 'errornot',
     response_type: 'code',
     redirect_uri: 'http://127.0.0.1:8888/login',
