5def83129ea3ce95f0f9a70f557b4ff28c506957
Virtuo <pierre@ruyssen.fr>
2010-11-18 10:46:54 +0100

    Remove deprecated stuff (rest API for web app). It might come back later differently.

--- src/server.js
+++ src/server.js
@@ -11,7 +11,6 @@
 , 'nodetk/src'
 , 'rest-mongo/src'
 , 'connect/lib'
-, 'dispatch/lib'
 , 'cookie-sessions/lib'
 , 'connect-form/lib'
 , 'mustache/lib'
@@ -22,59 +21,21 @@
 
 var connect = require('connect')
   , sessions = require('cookie-sessions')
-  , dispatch = require('dispatch')
   , connect_form = require('connect-form')
-  , URL = require('url')
-
-  , eyes = require('eyes')
 
   , CLB = require('nodetk/orchestration/callbacks')
   , rest_server = require('rest-mongo/http_rest/server')  
 
   , config = require('./config')
-  , oauth2 = require('./oauth2')
   , oauth2_server = require('./oauth2_server')
   , oauth2_resources_server = require('./oauth2_resources_server')  
   , oauth2_client = require('./oauth2_client')
   , web_app = require('./web_app')
   , authentication = require('./authentication')
-  , authorizations = require('./controllers/authorizations')
-  , users = require('./controllers/users')
-  , clients = require('./controllers/clients')
   , RFactory = require('./model').RFactory
   , schema = require('./schema').schema
   , ms_templates = require('./lib/ms_templates')
   ;
-
-
-var inspect = eyes.inspector({
-  maxLength: null
-});
-
-
-// ---------------------------------------------------------
-// Only for auth_server GUI:
-
-// TODO: DEPRECETED
-//gh.get('/authorizations', function(args) {
-//  var params = this.params || {}
-//    , client_ids = (params.clients)? params.clients.split(',') : []
-//    , user_ids = (params.user_ids)? params.users.split(',') : []
-//    , contexts = (params.contexts)? params.contexts.split(',') : []
-//  authorizations.get_authorizations(this, client_ids, user_ids, contexts);
-//});
-//
-//gh.get('/clients/{client_id}/contexts', clients.get_client_contexts);
-//
-//gh.get('/users', users.get_users);
-//gh.get('/clients', clients.get_clients);
-//gh.get('/clients/{client_id}', clients.get_client);
-//gh.post('/clients/{client_id}', clients.update_client);
-//
-//gh.get('/users/{user_id}/profile', function(user_id) {
-//  /* To get the profile information of the user.
-//   */
-//});
 
 
 var server = exports.server = connect.createServer(
