1f9d41be72f84cd85556e68de12bf278e54d6222
Virtuo <pierre@ruyssen.fr>
2011-01-25 16:04:47 +0100

    GUI: Can delete authorizations.

--- src/static/js/authorizations.js
+++ src/static/js/authorizations.js
@@ -2,7 +2,8 @@ var AuthServerAuthorizationLineView = Backbone.View.extend({
   events: {
     "click .roles": "edit_roles",
     "keypress .roles": "update_roles",
-    "focusout .roles": "save_roles"
+    "focusout .roles": "save_roles",
+    "click .selector": "selection"
   },
 
   tagName: "tr",
@@ -44,21 +45,54 @@ var AuthServerAuthorizationLineView = Backbone.View.extend({
 
   cancel_roles: function() {
     this.render();
+  },
+
+  selection: function(e) {
+    var selected = this.selected = e.target.checked;
+    $(this.el).toggleClass('selected', selected);
   }
 });
 
 var AuthServerAuthorizationsView = Backbone.View.extend({
+  events: {
+    "click input.delete": "del"
+  },
+
   render: function() {
     $('#overview').html('<h1>Authorizations</h1>');
 
     $(this.el).renders('authorizations_index');
     var tbody = [];
     _(this.model).each(function(authorization) {
-      var line = new AuthServerAuthorizationLineView({model: authorization}).render();
+      var line = new AuthServerAuthorizationLineView({
+        model: authorization
+      }).render();
       tbody.push(line.el);
+      authorization.view = line;
     });
     this.$('tbody').append(tbody);
     return this;
+  },
+
+  del: function() {
+    var self = this;
+    var to_delete = [], ids_to_delete = [], to_keep = [];
+    _(this.model).each(function(authorization) {
+      if(authorization.view.selected) {
+        to_delete.push(authorization);
+        ids_to_delete.push(authorization.id);
+      }
+      else to_keep.push(authorization);
+    });
+    R.Authorization.delete_({ids: ids_to_delete}, function() {
+      self.model = to_keep;
+      to_delete.forEach(function(authorization) {
+        $(authorization.view.el).remove();
+       });
+      console.log("Authorization(s) deleted.");
+    }, function(err) {
+      console.error('Could not delete the authorizations:', err);
+    });
   }
 });
 
